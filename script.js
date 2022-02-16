5d2b08c0294c2ada5e1a91f0656b6c024a5bcdf1
Alisherova Gulnaz <alisherovagulnaz268@gmail.com>
2022-02-16 14:38:55 +0600

    added if

--- script.js
+++ script.js
@@ -1,15 +1,12 @@
 document.querySelector('input[="text"]')
    .addEventListener('focus', function(){
-   this.value = "Hello";
    this.style.borderColor = "initial";
 });
 document.querySelector('input[type="text"]')
    .addEventListener('blur', function(){
      if (this.value) {
-
+      this.style.borderColor = "red";
      }
-   this.value = "Bye";
-   this.style.borderColor = "red";
 });
 
 //blur - check if it's empty. If empty highlight borders red.
