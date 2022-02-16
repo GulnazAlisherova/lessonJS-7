e4af24a68efa0a3952d2d3d923fa9808b23e8007
Alisherova Gulnaz <alisherovagulnaz268@gmail.com>
2022-02-16 14:38:05 +0600

    added borderColor

--- script.js
+++ script.js
@@ -1,12 +1,15 @@
 document.querySelector('input[="text"]')
    .addEventListener('focus', function(){
    this.value = "Hello";
-   this.style.border = "initial";
+   this.style.borderColor = "initial";
 });
 document.querySelector('input[type="text"]')
    .addEventListener('blur', function(){
+     if (this.value) {
+
+     }
    this.value = "Bye";
-   this.style.border = "red";
+   this.style.borderColor = "red";
 });
 
 //blur - check if it's empty. If empty highlight borders red.
