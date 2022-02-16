47ea2bb62c8c47e0c2f18699ff71dffc5ad70333
Alisherova Gulnaz <alisherovagulnaz268@gmail.com>
2022-02-16 15:17:15 +0600

    added addEventListener

--- script.js
+++ script.js
@@ -1,32 +1,36 @@
-document.querySelector('input[="text"]')
-   .addEventListener('focus', function(){
-   this.style.borderColor = "initial";
-});
-document.querySelector('input[type="text"]')
-   .addEventListener('blur', function(){
-     if (this.value) {
-      this.style.borderColor = "red";
-     }
-});
+// document.querySelector('input[="text"]')
+//    .addEventListener('focus', function(){
+//    this.style.borderColor = "initial";
+// });
+// document.querySelector('input[type="text"]')
+//    .addEventListener('blur', function(){
+//      if (this.value) {
+//       this.style.borderColor = "red";
+//      }
+// });
 
-//blur - check if it's empty. If empty highlight borders red.
-//Focus - remove red borders.
+// //blur - check if it's empty. If empty highlight borders red.
+// //Focus - remove red borders.
 
-document.querySelector('input[="text"]')
-   .addEventListener('change', function(){
-   alert('Changed');
-});
-document.querySelector('input[="checkbox"]')
-   .addEventListener('change', function(){
-   if (this.value){
-    alert('you are employed');
-   }
-   else{
-    alert('you are unemployed');
-   }
-});
+// document.querySelector('input[="text"]')
+//    .addEventListener('change', function(){
+//    alert('Changed');
+// });
+// document.querySelector('input[="checkbox"]')
+//    .addEventListener('change', function(){
+//    if (this.value){
+//     alert('you are employed');
+//    }
+//    else{
+//     alert('you are unemployed');
+//    }
+// });
+
 
 document.querySelector('input[type="text"]')
   .addEventListener('input', function(){
   console.log('input event triggered')
-  })
+});
+  
+//when select changes
+//show akert with the region for the city
