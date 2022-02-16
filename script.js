0ccd5cb52ddea3894b6c436f1c4be28ce1c0b278
Alisherova Gulnaz <alisherovagulnaz268@gmail.com>
2022-02-16 14:40:41 +0600

    added change

--- script.js
+++ script.js
@@ -11,3 +11,8 @@ document.querySelector('input[type="text"]')
 
 //blur - check if it's empty. If empty highlight borders red.
 //Focus - remove red borders.
+
+document.querySelector('input[="text"]')
+   .addEventListener('change', function(){
+   this.style.borderColor = "initial";
+});
